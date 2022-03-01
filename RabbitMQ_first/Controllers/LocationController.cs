b53851a985a0941f322e7495d1e094b66ffe4d16
zulyhallo <zuleyhaokur54@gmail.com>
2022-03-01 07:54:02 +0300

    Add project files.

---
+++ RabbitMQ_first/Controllers/LocationController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ_first.Models;
+using System.Text;
+
+namespace RabbitMQ_first.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        [HttpPost]
+        public void  Post ([FromBody]Location location)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost:44368" };
+            using (var connection =factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "locationSampleQueue",
+                                            durable: false,
+                                            exclusive: false,
+                                            autoDelete: false,
+                                            arguments: null
+                                            );
+
+                string message = "Latitude: " + location.Latitude + ", Longitude: " + location.Longitude + " and Time: " + location.Date;
+                var body = Encoding.UTF8.GetBytes(message);
+
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "locationSampleQueue",
+                                     basicProperties: null,
+                                     body: body);
+
+            }
+
+        }
+
+
+    }
+}
---
+++ RabbitMQ_first/Models/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RabbitMQ_first.Models
+{
+    //
+
+    public class Location
+    {
+        public DateTime Date { get; set; }
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+    }
+}
